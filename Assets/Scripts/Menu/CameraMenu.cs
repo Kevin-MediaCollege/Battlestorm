b1d4af51d9ca9192ab3bd40cfabb722498294856
kevinness <bioniclefreak_19@hotmail.com>
2013-12-10 11:51:25 +0100

    PLZ NO DETACHED HEAD

---
+++ Assets/Scripts/Menu/CameraMenu.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMenu : MonoBehaviour {
+	public GameObject Point1;
+	public GameObject Point2;
+
+	public Vector3 startPoint;
+	public Vector3 endPoint;
+
+	public float duration = 5.0f;
+	private float startTime;
+	// Use this for initialization
+	void Start () {
+		startPoint = Point1.transform.position;
+		endPoint = Point2.transform.position;
+		startTime = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = Vector3.Lerp(startPoint, endPoint, (Time.time - startTime) / duration);
+	}
+}
