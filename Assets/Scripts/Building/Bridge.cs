df8361f7e201bdbf17e90617635dfeec4726e52f
kevinness <bioniclefreak_19@hotmail.com>
2013-12-11 20:04:45 +0100

    PLZ WERK

---
+++ Assets/Scripts/Building/Bridge.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bridge : MonoBehaviour {
+	public int bridgeParts;
+	public GameObject spawnposition;
+
+	public bool beenMade;
+	// Use this for initialization
+	void Start () {
+		BuildBridge();
+	}
+	
+	public void BuildBridge(){
+		for(int i = 0; i < bridgeParts; i++){
+			Vector3 spawnpos = spawnposition.transform.position + (Vector3.forward * 0.5f);
+			GameObject part = Instantiate(Resources.Load("Prefabs/Buildings/BridgePart"),spawnpos,transform.rotation) as GameObject;
+			part.transform.parent = spawnposition.transform;
+			part.name = "BridgePart";
+			part.transform.position += Vector3.forward * i;
+			beenMade = true;
+		}
+	}
+}
