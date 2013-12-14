5370353992a95a06edb64033ab9e5d6b48f8a137
kevinness <bioniclefreak_19@hotmail.com>
2013-12-14 18:37:12 +0100

    WaveSystem Added

--- Assets/Scripts/Enemy/EnemyManager.cs
+++ Assets/Scripts/Enemy/EnemyManager.cs
@@ -1,26 +1,31 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+using System.Collections.Generic;
 public class EnemyManager:MonoBehaviour {
 	public Transform parent;
 	public Transform start;
 	public Transform end;
-
+	
 	public float spawnDelay;
-
+	
 	private GameObject enemy;
-
+	
+	public GameObject[] enemies;
+	public List<GameObject> enemyList;
 	void Start() {
-		StartCoroutine("spawner");
+		enemyList = new List<GameObject>();
 	}
-
-	IEnumerator spawner() {
-		while(true) {
-			yield return new WaitForSeconds(spawnDelay);
-
-			enemy = Instantiate(Resources.Load("Prefabs/Entity/Enemy"), start.position, Quaternion.identity) as GameObject;
-			enemy.GetComponent<PathFollower>().target = end;
-			enemy.transform.parent = parent;
+	void FixedUpdate(){
+		for(int i = 0; i < enemyList.Count; i++){
+			if(enemyList[i].gameObject == null){
+				enemyList.RemoveAt(i);
+			}
 		}
+	}
+	public void spawnEnemy() {
+		enemy = Instantiate(Resources.Load("Prefabs/Entity/Enemy"), start.position, Quaternion.identity) as GameObject;
+		enemy.GetComponent<PathFollower>().target = end;
+		enemy.transform.parent = parent;
+		enemyList.Add(enemy.transform.gameObject);
 	}
 }
---
+++ Assets/Scripts/Game/GameManager.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+	private EnemyManager eManager;
+	
+	private int currentWave = 0;
+	//TODO use xml or something to load the wave amounts
+	private int[] wavelength = {3,5,7,10,12,24,30};
+	
+	private int enemiesToSpawn;
+	private bool allSpawned;
+	
+	private int waveTime = 35;
+	private string waveTimer;
+	private bool timerStarted = false;
+	// Use this for initialization
+	void Start () {
+		eManager = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<EnemyManager>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if(allSpawned && eManager.enemyList.Count == 0 && !timerStarted){
+			timerStarted = true;
+			StartCoroutine("spawnTimer");
+		}
+	}
+	void Nextwave(){
+		currentWave++;
+		allSpawned = false;
+		timerStarted = false;
+		enemiesToSpawn = wavelength[currentWave];
+		StartCoroutine("spawnEnemies");
+	}
+	IEnumerator spawnTimer(){
+		if(waveTime != 0){
+			yield return new WaitForSeconds(1);
+			waveTime--;
+			waveTimer = "" + waveTime;
+			StartCoroutine("spawnTimer");
+		}
+		else{
+			waveTimer = "";
+			Nextwave();
+		}
+	}
+	IEnumerator spawnEnemies(){
+		yield return new WaitForSeconds(1.8f);
+		if(enemiesToSpawn != 0){
+			waveTimer = "";
+			eManager.spawnEnemy();
+			enemiesToSpawn--;
+			StartCoroutine("spawnEnemies");
+		}
+		else{
+			waveTime = 35;
+			allSpawned = true;
+		}
+	}
+	void OnGUI(){
+		//Test GUI
+		if(GUI.Button(new Rect(0,0,50,50),"Next Wave")){
+			Debug.Log(eManager.enemyList.Count);
+			if(eManager.enemyList.Count == 0 && enemiesToSpawn == 0){
+				StopCoroutine("spawnTimer");
+				waveTimer = "";
+				Nextwave();
+			}
+		}
+		GUI.Box(new Rect(50,0,50,50),waveTimer);
+	}
+}
