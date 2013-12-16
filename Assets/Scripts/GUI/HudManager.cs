4e725ebbd9c2ff57ceddd35e4e7924d4b91c6278
Snakybo (Laptop) <krol.kevin@gmail.com>
2013-12-16 13:16:43 +0100

    i no want das

---
+++ Assets/Scripts/GUI/HudManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HudManager:MonoBehaviour {
+	public Texture texture;
+	public Vector2 position;
+
+	private int textureX;
+
+	void OnGUI() {
+		textureX = texture.width / 2;
+
+		GUI.DrawTexture(new Rect(position.x * textureX, position.y * Screen.height, texture.width, texture.height), texture);
+
+		Debug.Log(position.x * textureX);
+	}
+}
