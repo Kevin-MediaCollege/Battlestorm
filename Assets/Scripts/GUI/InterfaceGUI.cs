04a45c9dcf4f8fd8f081fc3624ad5b8675950e8b
Snakybo (Laptop) <krol.kevin@gmail.com>
2013-12-20 09:47:38 +0100

    I HAVE NO CLUE

--- Assets/Scripts/GUI/InterfaceGUI.cs
+++ Assets/Scripts/GUI/InterfaceGUI.cs
@@ -3,13 +3,19 @@ using System.Collections;
 
 public class InterfaceGUI:MonoBehaviour {
 	public Texture texture;
+	public Texture button;
+	public Font font;
 
 	void OnGUI() {
+		float center = (Screen.width / 2) - (texture.width / 2);
+
 		float rX = Screen.width / GameManager.nativeWidth;
 		float rY = Screen.height / GameManager.nativeHeight;
 
 		GUI.matrix = Matrix4x4.TRS(new Vector3(0, 0, 0), Quaternion.identity, new Vector3(rX, rY, 1));
 
-		GUI.DrawTexture(new Rect((Screen.width / 2) - (texture.width / 2), 0, texture.width, texture.height), texture);
+		GUI.BeginGroup(new Rect(center, 0, texture.width, texture.height), new GUIContent(texture));
+			GUI.DrawTexture(new Rect(center + 16, 0, button.width, button.height), button);
+		GUI.EndGroup();
 	}
 }
