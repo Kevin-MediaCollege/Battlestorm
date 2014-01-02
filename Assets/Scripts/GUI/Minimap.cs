05b8245bddc071029a74e2a43d0e224b96a388aa
kevinness <bioniclefreak_19@hotmail.com>
2014-01-02 22:29:00 +0100

    Zooming Minimap

---
+++ Assets/Scripts/GUI/Minimap.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Minimap : MonoBehaviour {
+	public bool openmap = false;
+	public Vector2 mapsize;
+	// Use this for initialization
+	void Start () {
+		mapsize.x = 8;
+		mapsize.y = 7;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		gameObject.camera.pixelRect =new Rect(0,0,Screen.width / mapsize.x,Screen.height / mapsize.y);
+	if(Input.GetKey(KeyCode.Tab)){
+			openmap = true;
+		}
+		else{
+			openmap = false;
+		}
+		if(openmap){
+			if(mapsize.x > 1.5f){
+				mapsize.x -= 0.1f;
+			}
+			if(mapsize.y > 1.5f){
+				mapsize.y -= 0.1f;
+			}
+		}
+		else{
+			if(mapsize.x < 8){
+				mapsize.x += 0.1f;
+			}
+			if(mapsize.y < 7){
+				mapsize.y += 0.1f;
+			}
+		}
+	}
+
+}
