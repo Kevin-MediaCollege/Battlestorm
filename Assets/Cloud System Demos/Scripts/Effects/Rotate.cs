ceec2d450ecad283a711b5a1dd3af9d82fe238ea
kevinness <bioniclefreak_19@hotmail.com>
2014-01-16 09:44:10 +0100

    SafetyPush

--- Assets/Cloud System Demos/Scripts/Effects/Rotate.cs
+++ Assets/Cloud System Demos/Scripts/Effects/Rotate.cs
@@ -11,8 +11,12 @@ using System.Collections;
 public class Rotate : MonoBehaviour {
 
 	public float duration = 100.0f;
-	
+	public bool invert;
 	private void Update () {
+		if(invert){
+			transform.Rotate (Vector3.forward, 360.0f * Mathf.Deg2Rad / duration * Time.deltaTime);
+		}else{
 		transform.Rotate (Vector3.back, 360.0f * Mathf.Deg2Rad / duration * Time.deltaTime);
+		}
 	}
 }
