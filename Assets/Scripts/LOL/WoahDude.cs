bf646aa93c69d2ed531e9464cbeda9daa236dc45
kevinness <bioniclefreak_19@hotmail.com>
2014-01-16 19:51:52 +0100

    WoahDude

---
+++ Assets/Scripts/LOL/WoahDude.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WoahDude : MonoBehaviour {
+	public Camera cam;
+	public Light disco;
+	public bool zoom;
+	public bool hasstarted = false;
+	public AudioSource one;
+	public AudioSource two;
+
+	public AudioClip badger;
+	public AudioClip wavemusic;
+	public AudioClip waitmusic;
+
+	public Color ambient;
+	public Color lightcol;
+	// Use this for initialization
+	void Start () {
+		lightcol = disco.color;
+		ambient = RenderSettings.ambientLight;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.F2) && hasstarted){
+			StopCoroutine("WoahDisco");
+			cam.fieldOfView = 70;
+			hasstarted = false;
+			two.Stop();
+			one.Stop();
+			disco.intensity = 0.45f;
+			RenderSettings.ambientLight = ambient;
+			disco.color = lightcol;
+			one.clip = waitmusic;
+			two.clip = wavemusic;
+			two.Play();
+			one.Play();
+		}
+		if(Input.GetKey(KeyCode.F1) && !hasstarted){
+			hasstarted = true;
+			two.Stop();
+			one.Stop();
+			StartCoroutine("WoahDisco");
+			disco.intensity = 8;
+			one.clip = badger;
+			two.clip = badger;
+			two.Play();
+			one.Play();
+		}
+		if(hasstarted){
+			if(cam.fieldOfView < 70){
+				zoom = true;
+			}
+			if(cam.fieldOfView > 140){
+				zoom = false;
+			}
+			if(zoom){
+				cam.fieldOfView += 4;
+			}else{
+				cam.fieldOfView -= 4;
+			}
+		}
+	}
+
+	IEnumerator WoahDisco(){
+		yield return new WaitForSeconds(0.5f);
+		disco.color =  new Color((float)Random.Range(0.0f,1.0f),(float)Random.Range(0.0f,1.0f),(float)Random.Range(0.0f,1.0f));
+		RenderSettings.ambientLight = new Color((float)Random.Range(0.0f,1.0f),(float)Random.Range(0.0f,1.0f),(float)Random.Range(0.0f,1.0f));
+		StartCoroutine("WoahDisco");
+	}
+}
