6c0f4f729a4a63b61a7275063bb69b0abdd00148
Snakybo Laptop <krol.kevin@gmail.com>
2014-02-17 13:59:11 +0100

    Added school version

--- Assets/Scripts/Menu/MenuGUI.cs
+++ Assets/Scripts/Menu/MenuGUI.cs
@@ -14,43 +14,46 @@ public class MenuGUI:MonoBehaviour {
 	public Texture2D buttonBackOn;
 	public Texture2D buttonDownloadNormal;
 	public Texture2D buttonDownloadOn;
-
+	
+	public Texture logo;
+	public Texture jasonLogo;
+	
 	public string standaloneUrl;
-
+	
 	public GUIStyle buttonStyle;
-
+	
 	public AudioClip buttonHover;
 	public AudioClip buttonClick;
-
+	
 	public Camera optionCamera;
 	public Camera creditsCamera;
-
+	
 	public FadeScript fade;
-
+	
 	public CameraMenu flybyCamera;
 	
 	public Texture optionBackground;
-
+	
 	public GUIStyle optionStyle;
 	public GUIStyle optionindexStyle;
 	public GUIStyle optionToggle;
 	public GUIStyle sliderStyle;
 	public GUIStyle thumbStyle;
 	public GUIStyle inputStyle;
-
+	
 	public Vector2[] resolutions;
 	public Vector2 currentresolution;
 	
 	public Light ingameLight;
-
+	
 	public AudioSource music;
-
+	
 	public int optionIndex;
 	public int keyCodeNum;
 	public int selectedresolutions;
-
+	
 	private KeyCode selectedKeyCode;
-
+	
 	private bool offsetBool;
 	private bool givingInput;
 	private bool openMainMenu;
@@ -59,14 +62,14 @@ public class MenuGUI:MonoBehaviour {
 	
 	private string lastGUITooltip;
 	private string tooltip;
-
+	
 	void Start () {
 		Time.timeScale = 1.0f;
 		selectedresolutions = 3;
 		currentresolution = resolutions[selectedresolutions];
 		openMainMenu = true;
 	}
-
+	
 	void FixedUpdate() {
 		if(optionIndex == -1){
 			optionIndex = 3;
@@ -99,7 +102,7 @@ public class MenuGUI:MonoBehaviour {
 			playSound (2);
 			lastGUITooltip = tooltip;
 		}
-
+		
 		if(tooltip == "") {
 			lastGUITooltip = "NoTooltip";
 		}
@@ -110,7 +113,7 @@ public class MenuGUI:MonoBehaviour {
 		flybyCamera.stopMoving = false;
 		this.camera.enabled = true;
 	}
-
+	
 	void playSound(int sound) {
 		if (!audio.isPlaying) {
 			switch (sound) {
@@ -135,9 +138,11 @@ public class MenuGUI:MonoBehaviour {
 		float rx = Screen.width / GameManager.nativeWidth;
 		float ry = Screen.height / GameManager.nativeHeight;
 		GUI.matrix = Matrix4x4.TRS (new Vector3(0, 0, 0), Quaternion.identity, new Vector3 (rx, ry, 1)); 
-
-
+		
 		if(openMainMenu){
+			GUI.DrawTexture(new Rect(500, 50, 296, 240), logo);
+			GUI.DrawTexture(new Rect(930, 15, 334, 181), jasonLogo);
+
 			buttonStyle.normal.background = buttonPlayNormal;
 			buttonStyle.hover.background = buttonPlayOn;
 			buttonStyle.active.background = buttonPlayOn;
@@ -145,46 +150,26 @@ public class MenuGUI:MonoBehaviour {
 				playSound(1);
 				LoadingScreen.Instance.loadLoadingScreen("GameMap");
 			}
-
+			
 			buttonStyle.normal.background = buttonOptionsNormal;
 			buttonStyle.hover.background = buttonOptionsOn;
 			buttonStyle.active.background = buttonOptionsOn;
-			if(GUI.Button(new Rect(316, 650, 256, 49), new GUIContent("", "Options"), buttonStyle)){
+			if(GUI.Button(new Rect(515, 650, 256, 49), new GUIContent("", "Options"), buttonStyle)){
 				playSound(1);
 				fade.fadingOut = true;
 				openOptions = true;
 			}
-
+			
 			buttonStyle.normal.background = buttonCreditsNormal;
 			buttonStyle.hover.background = buttonCreditsOn;
 			buttonStyle.active.background = buttonCreditsOn;
-			if(GUI.Button(new Rect(719, 650, 256, 49), new GUIContent("", "Credits"), buttonStyle)){
+			if(GUI.Button(new Rect(1005, 650, 256, 49), new GUIContent("", "Credits"), buttonStyle)){
 				playSound(1);
 				fade.fadingOut = true;
 				openCredits = true;
 			}
-
+			
 			tooltip = GUI.tooltip;
-
-			if(!Application.isWebPlayer) {
-				buttonStyle.normal.background = buttonQuitNormal;
-				buttonStyle.hover.background = ButtonQuitOn;
-				buttonStyle.active.background = ButtonQuitOn;
-
-				if(GUI.Button(new Rect(1005, 650, 256, 49), new GUIContent("", "Quit"), buttonStyle)){
-					playSound(1);
-					Application.Quit();
-				}
-			} else {
-				buttonStyle.normal.background = buttonDownloadNormal;
-				buttonStyle.hover.background = buttonDownloadOn;
-				buttonStyle.active.background = buttonDownloadOn;
-
-				if(GUI.Button(new Rect(1005, 650, 256, 49), new GUIContent("", "DownloadSA"), buttonStyle)) {
-					playSound(1);
-					Application.ExternalEval("window.open('" + standaloneUrl + "','Download Battle Storm Standalone')");
-				}
-			}
 		}
 		if(!openMainMenu && !openOptions){
 			if(GUI.Button(new Rect(550,650,256,49),"Back",optionindexStyle)){
@@ -203,15 +188,15 @@ public class MenuGUI:MonoBehaviour {
 				fade.fadingOut = true;
 				StartCoroutine("delay");
 			}
-
+			
 			if(GUI.Button(new Rect(400,0,50,720),"",optionindexStyle)){
 				optionIndex -= 1;
-
+				
 			}
 			if(GUI.Button(new Rect(850,0,50,720),"",optionindexStyle)){
 				optionIndex += 1;
 			}
-
+			
 			if(optionIndex == 0){
 				//INPUT 0
 				DrawMainOptionsWindow();
@@ -233,10 +218,10 @@ public class MenuGUI:MonoBehaviour {
 		optionStyle.fontSize = 20;
 		GUI.Label(new Rect(600,150,100,50),"Music",optionStyle);
 		VolumeManager.MusicVolume = GUI.HorizontalSlider(new Rect(550, 195, 200, 15), VolumeManager.MusicVolume, 0.0F, 1.0F,sliderStyle,thumbStyle);
-
+		
 		GUI.Label(new Rect(600,230,100,50),"Sound Effects",optionStyle);
 		VolumeManager.SoundVolume = GUI.HorizontalSlider(new Rect(550, 275, 200, 15), VolumeManager.SoundVolume, 0.0F, 1.0F,sliderStyle,thumbStyle);
-
+		
 	}
 	void DrawMainOptionsWindow(){
 		optionStyle.fontSize = 40;
@@ -249,40 +234,40 @@ public class MenuGUI:MonoBehaviour {
 	}
 	void DrawInputWindow(){
 		if(givingInput){
-				Event e = Event.current;
-				if (e.isKey){
+			Event e = Event.current;
+			if (e.isKey){
 				Debug.Log("Detected key code: " + e.keyCode);
 				selectedKeyCode = e.keyCode;
-
+				
 				switch(keyCodeNum){
-					case 0:
-						InputHandler.left = selectedKeyCode;
+				case 0:
+					InputHandler.left = selectedKeyCode;
 					break;
-					case 1:
-						InputHandler.right = selectedKeyCode;
+				case 1:
+					InputHandler.right = selectedKeyCode;
 					break;
-					case 2:
-						InputHandler.forward = selectedKeyCode;
+				case 2:
+					InputHandler.forward = selectedKeyCode;
 					break;
-					case 3:
-						InputHandler.back = selectedKeyCode;
+				case 3:
+					InputHandler.back = selectedKeyCode;
 					break;
-					case 4:
-						InputHandler.up = selectedKeyCode;
+				case 4:
+					InputHandler.up = selectedKeyCode;
 					break;
-					case 5:
-						InputHandler.down = selectedKeyCode;
+				case 5:
+					InputHandler.down = selectedKeyCode;
 					break;
-					case 6:
-						InputHandler.minimap = selectedKeyCode;
+				case 6:
+					InputHandler.minimap = selectedKeyCode;
 					break;
-					case 7:
-						InputHandler.buildingManager = selectedKeyCode;
+				case 7:
+					InputHandler.buildingManager = selectedKeyCode;
 					break;
 				}
 				givingInput = false;
-				}
 			}
+		}
 		if(givingInput){
 			inputStyle.fontSize = 65;
 			GUI.Label(new Rect(465,220,100,50),"Press any key",inputStyle);
@@ -292,98 +277,98 @@ public class MenuGUI:MonoBehaviour {
 			optionStyle.fontSize = 40;
 			GUI.Label(new Rect(600,25,100,50),"Input",optionStyle);
 			optionStyle.fontSize = 20;
-		GUI.BeginGroup(new Rect(460,100,400,50),"",optionStyle);
+			GUI.BeginGroup(new Rect(460,100,400,50),"",optionStyle);
 			GUI.DrawTexture(new Rect(0,0,300,50),optionBackground);
 			GUI.Label(new Rect(5,0,100,50),"Movement - Left  =",inputStyle);
 			GUI.Label(new Rect(200,0,100,50),"" + InputHandler.left.ToString(),optionStyle);
-
+			
 			optionindexStyle.fontSize = 20;
 			if(GUI.Button(new Rect(320,0,50,50),"Set",optionindexStyle)){
 				givingInput = true;
 				keyCodeNum = 0;
 			}
 			optionindexStyle.fontSize = 40;
-		GUI.EndGroup();
-
-		GUI.BeginGroup(new Rect(460,160,400,50),"",optionStyle);
+			GUI.EndGroup();
+			
+			GUI.BeginGroup(new Rect(460,160,400,50),"",optionStyle);
 			GUI.DrawTexture(new Rect(0,0,300,50),optionBackground);
 			GUI.Label(new Rect(5,0,100,50),"Movement - Right  =",inputStyle);
 			GUI.Label(new Rect(200,0,100,50),"" + InputHandler.right.ToString(),optionStyle);
-
+			
 			optionindexStyle.fontSize = 20;
 			if(GUI.Button(new Rect(320,0,50,50),"Set",optionindexStyle)){
 				givingInput = true;
 				keyCodeNum = 1;
 			}
 			optionindexStyle.fontSize = 40;
-		GUI.EndGroup();
-
-		GUI.BeginGroup(new Rect(460,220,400,50),"",optionStyle);
+			GUI.EndGroup();
+			
+			GUI.BeginGroup(new Rect(460,220,400,50),"",optionStyle);
 			GUI.DrawTexture(new Rect(0,0,300,50),optionBackground);
 			GUI.Label(new Rect(5,0,100,50),"Movement - Forward  =",inputStyle);
 			GUI.Label(new Rect(200,0,100,50),"" + InputHandler.forward.ToString(),optionStyle);
-
+			
 			optionindexStyle.fontSize = 20;
 			if(GUI.Button(new Rect(320,0,50,50),"Set",optionindexStyle)){
 				givingInput = true;
 				keyCodeNum = 2;
 			}
 			optionindexStyle.fontSize = 40;
-		GUI.EndGroup();
-
-		GUI.BeginGroup(new Rect(460,280,400,50),"",optionStyle);
+			GUI.EndGroup();
+			
+			GUI.BeginGroup(new Rect(460,280,400,50),"",optionStyle);
 			GUI.DrawTexture(new Rect(0,0,300,50),optionBackground);
 			GUI.Label(new Rect(5,0,100,50),"Movement - Backwards  =",inputStyle);
 			GUI.Label(new Rect(200,0,100,50),"" + InputHandler.back.ToString(),optionStyle);
-
+			
 			optionindexStyle.fontSize = 20;
 			if(GUI.Button(new Rect(320,0,50,50),"Set",optionindexStyle)){
 				givingInput = true;
 				keyCodeNum = 3;
 			}
 			optionindexStyle.fontSize = 40;
-		GUI.EndGroup();
-
-		GUI.BeginGroup(new Rect(460,340,400,50),"",optionStyle);
+			GUI.EndGroup();
+			
+			GUI.BeginGroup(new Rect(460,340,400,50),"",optionStyle);
 			GUI.DrawTexture(new Rect(0,0,300,50),optionBackground);
 			GUI.Label(new Rect(5,0,100,50),"Movement - Up  =",inputStyle);
 			GUI.Label(new Rect(200,0,100,50),"" + InputHandler.up.ToString(),optionStyle);
-
+			
 			optionindexStyle.fontSize = 20;
 			if(GUI.Button(new Rect(320,0,50,50),"Set",optionindexStyle)){
 				givingInput = true;
 				keyCodeNum = 4;
 			}
 			optionindexStyle.fontSize = 40;
-		GUI.EndGroup();
-
-		GUI.BeginGroup(new Rect(460,400,400,50),"",optionStyle);
+			GUI.EndGroup();
+			
+			GUI.BeginGroup(new Rect(460,400,400,50),"",optionStyle);
 			GUI.DrawTexture(new Rect(0,0,300,50),optionBackground);
 			GUI.Label(new Rect(5,0,100,50),"Movement - Down  =",inputStyle);
 			GUI.Label(new Rect(200,0,100,50),"" + InputHandler.down.ToString(),optionStyle);
-
+			
 			optionindexStyle.fontSize = 20;
 			if(GUI.Button(new Rect(320,0,50,50),"Set",optionindexStyle)){
 				givingInput = true;
 				keyCodeNum = 5;
 			}
 			optionindexStyle.fontSize = 40;
-		GUI.EndGroup();
-
-		GUI.BeginGroup(new Rect(460,460,400,50),"",optionStyle);
+			GUI.EndGroup();
+			
+			GUI.BeginGroup(new Rect(460,460,400,50),"",optionStyle);
 			GUI.DrawTexture(new Rect(0,0,300,50),optionBackground);
 			GUI.Label(new Rect(5,0,100,50),"Minimap Key  =",inputStyle);
 			GUI.Label(new Rect(200,0,100,50),"" + InputHandler.minimap.ToString(),optionStyle);
-		
+			
 			optionindexStyle.fontSize = 20;
 			if(GUI.Button(new Rect(320,0,50,50),"Set",optionindexStyle)){
 				givingInput = true;
 				keyCodeNum = 6;
 			}
 			optionindexStyle.fontSize = 40;
-		GUI.EndGroup();
-
-		GUI.BeginGroup(new Rect(460,520,400,50),"",optionStyle);
+			GUI.EndGroup();
+			
+			GUI.BeginGroup(new Rect(460,520,400,50),"",optionStyle);
 			GUI.DrawTexture(new Rect(0,0,300,50),optionBackground);
 			GUI.Label(new Rect(5,0,100,50),"BuildingManager =",inputStyle);
 			GUI.Label(new Rect(200,0,100,50),"" + InputHandler.buildingManager.ToString(),optionStyle);
@@ -394,77 +379,61 @@ public class MenuGUI:MonoBehaviour {
 				keyCodeNum = 7;
 			}
 			optionindexStyle.fontSize = 40;
-		GUI.EndGroup();
-
-		GUI.Label(new Rect(485, 590, 50, 50), "Custom input is not implemented yet.", inputStyle);
+			GUI.EndGroup();
+			
+			GUI.Label(new Rect(485, 590, 50, 50), "Custom input is not implemented yet.", inputStyle);
 		}
 	}
 	void DrawGraphicsWindow(){ 
-		GUI.Label(new Rect(600,25,100,50),"Graphics",optionStyle);
-		GUI.Label(new Rect(530,80,100,50),"Fullscreen",optionStyle);
-		Screen.fullScreen = GUI.Toggle(new Rect(710,95,75,25),Screen.fullScreen,"",optionToggle);
-		
-		GUI.Label(new Rect(600,140,100,50),"Resolution",optionStyle);
-		GUI.Label(new Rect(600,175,100,50),"" + currentresolution.x +":" + currentresolution.y,optionStyle);
-		if(GUI.Button(new Rect(700,230,70,50),"+",optionindexStyle)){
-			if(selectedresolutions == 3){}else{
-				selectedresolutions += 1;
-			}
-			currentresolution = resolutions[selectedresolutions];
-		}
-		if(GUI.Button(new Rect(520,230,70,50),"-",optionindexStyle)){
-			if(selectedresolutions == 0){}else{
-				selectedresolutions -= 1;
-			}
-			currentresolution = resolutions[selectedresolutions];
-		}
-		optionindexStyle.fontSize = 20;
-		if(GUI.Button(new Rect(610,230,70,50),"Apply",optionindexStyle)){
-			Screen.SetResolution(Mathf.FloorToInt(currentresolution.x),Mathf.FloorToInt(currentresolution.y),Screen.fullScreen);
-		}
-		optionindexStyle.fontSize = 40;
+		int oldSize = optionStyle.fontSize;
 
+		optionStyle.fontSize = 40;
+		GUI.Label(new Rect(600,25,100,50),"Graphics",optionStyle);
+
+		optionStyle.fontSize = oldSize;
+		optionindexStyle.fontSize = 40;
+		
 		//Anti Aliasing
 		GUI.BeginGroup(new Rect(470,300,120,100),"",optionStyle);
-			GUI.DrawTexture(new Rect(0,0,120,100),optionBackground);
-			GUI.Label(new Rect(10,-5,100,50),"Anti-Aliasing",optionStyle);
-			QualitySettings.antiAliasing = Mathf.RoundToInt(GUI.HorizontalSlider(new Rect(5, 65, 90, 15), QualitySettings.antiAliasing, 0.0F, 8.0F,sliderStyle,thumbStyle));
+		GUI.DrawTexture(new Rect(0,0,120,100),optionBackground);
+		GUI.Label(new Rect(10,-5,100,50),"Anti-Aliasing",optionStyle);
+		QualitySettings.antiAliasing = Mathf.RoundToInt(GUI.HorizontalSlider(new Rect(5, 65, 90, 15), QualitySettings.antiAliasing, 0.0F, 8.0F,sliderStyle,thumbStyle));
 		GUI.EndGroup();
-
+		
 		//Texture Quality
 		GUI.BeginGroup(new Rect(470,410,120,100),"",optionStyle);
-			GUI.DrawTexture(new Rect(0,0,120,100),optionBackground);
-			GUI.Label(new Rect(10,-5,100,50),"Texture",optionStyle);
-			GUI.Label(new Rect(10,10,100,50),"Quality",optionStyle);
-			QualitySettings.masterTextureLimit = Mathf.RoundToInt(GUI.HorizontalSlider(new Rect(5, 65, 90, 15), QualitySettings.masterTextureLimit, 3.0F, 0.0F,sliderStyle,thumbStyle));
+		GUI.DrawTexture(new Rect(0,0,120,100),optionBackground);
+		GUI.Label(new Rect(10,-5,100,50),"Texture",optionStyle);
+		GUI.Label(new Rect(10,10,100,50),"Quality",optionStyle);
+		QualitySettings.masterTextureLimit = Mathf.RoundToInt(GUI.HorizontalSlider(new Rect(5, 65, 90, 15), QualitySettings.masterTextureLimit, 3.0F, 0.0F,sliderStyle,thumbStyle));
 		GUI.EndGroup();
-
+		
 		GUI.BeginGroup(new Rect(700,300,120,100),"",optionStyle);
-			GUI.DrawTexture(new Rect(0,0,120,100),optionBackground);
-			GUI.Label(new Rect(10,-5,100,50),"Anisotropic",optionStyle);
-			optionindexStyle.fontSize = 20;
+		GUI.DrawTexture(new Rect(0,0,120,100),optionBackground);
+		GUI.Label(new Rect(10,-5,100,50),"Anisotropic",optionStyle);
+		optionindexStyle.fontSize = 20;
 		if(GUI.Button(new Rect(60,45,50,50),"On",optionindexStyle)){
-				QualitySettings.anisotropicFiltering = AnisotropicFiltering.Enable;
-			}
+			QualitySettings.anisotropicFiltering = AnisotropicFiltering.Enable;
+		}
 		if(GUI.Button(new Rect(5,45,50,50),"Off",optionindexStyle)){
-				QualitySettings.anisotropicFiltering = AnisotropicFiltering.Disable;
-			}
-			optionindexStyle.fontSize = 40;
+			QualitySettings.anisotropicFiltering = AnisotropicFiltering.Disable;
+		}
+		optionindexStyle.fontSize = 40;
 		GUI.EndGroup();
-
+		
 		GUI.BeginGroup(new Rect(700,410,120,100),"",optionStyle);
-			GUI.DrawTexture(new Rect(0,0,120,100),optionBackground);
-			GUI.Label(new Rect(10,-5,100,50),"Fog",optionStyle);
-			optionindexStyle.fontSize = 20;
-			if(GUI.Button(new Rect(60,45,50,50),"On",optionindexStyle)){
-				RenderSettings.fog = true;
-			}
-			if(GUI.Button(new Rect(5,45,50,50),"Off",optionindexStyle)){
-				RenderSettings.fog = false;
-			}
-			optionindexStyle.fontSize = 40;
+		GUI.DrawTexture(new Rect(0,0,120,100),optionBackground);
+		GUI.Label(new Rect(10,-5,100,50),"Fog",optionStyle);
+		optionindexStyle.fontSize = 20;
+		if(GUI.Button(new Rect(60,45,50,50),"On",optionindexStyle)){
+			RenderSettings.fog = true;
+		}
+		if(GUI.Button(new Rect(5,45,50,50),"Off",optionindexStyle)){
+			RenderSettings.fog = false;
+		}
+		optionindexStyle.fontSize = 40;
 		GUI.EndGroup();
-
+		
 		GUI.DrawTexture(new Rect(500,550,280,110),optionBackground);
 		GUI.Label(new Rect(590,555,100,50),"Field of View",optionStyle);
 		GUI.Label(new Rect(590,575,100,50),"" + InputHandler.FOV,optionStyle);
